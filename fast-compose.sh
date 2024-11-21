44e80734784734cc6b3fdfde6ad148d1dae81df3
Ignacio Shishko <shishkoignacio@gmail.com>
2024-11-21 17:18:19 -0300

    v2

--- fast-compose.sh
+++ fast-compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Construye los contenedores definidos en el archivo docker-compose.yml sin utilizar la cache (--no-cache).
-sudo docker compose build #| tee docker compose.log
-sudo docker compose up #| tee -a docker-compose.log
+ docker compose build #| tee docker compose.log
+ docker compose up #| tee -a docker-compose.log
--- up-compose.sh
+++ up-compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Construye los contenedores definidos en el archivo docker-compose.yml sin utilizar la cache (--no-cache).
-sudo docker compose build --no-cache #| tee docker compose.log
-sudo docker compose up #| tee -a docker-compose.log
+ docker compose build --no-cache #| tee docker compose.log
+ docker compose up #| tee -a docker-compose.log
