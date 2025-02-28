8fe9b063b216535f1529ae7cd0b67d4115e80ee5
Ignacio Shishko <shishkoignacio@gmail.com>
2025-02-28 18:19:30 -0300

    v1.00

--- up.sh
+++ up.sh
@@ -20,14 +20,14 @@ docker compose up -d #| tee -a docker-compose.log
 
 # Recupera archivos de volumenes y elimina directorio temporal
 echo "Copiando archivos de modulos..."
-docker exec odoo17 cp -rf /mnt2/. /mnt
-docker exec odoo17 rm -rf /mnt2 
+docker exec odoo17ce2025 cp -rf /mnt2/. /mnt
+docker exec odoo17ce2025 rm -rf /mnt2 
 echo "Copiando archivos base..."
-docker exec odoo17 cp -rf /usr/lib/python3/dist-packages/odoo/addons2/. /usr/lib/python3/dist-packages/odoo/addons
-docker exec odoo17 rm -rf /usr/lib/python3/dist-packages/odoo/addons2
+docker exec odoo17ce2025 cp -rf /usr/lib/python3/dist-packages/odoo/addons2/. /usr/lib/python3/dist-packages/odoo/addons
+docker exec odoo17ce2025 rm -rf /usr/lib/python3/dist-packages/odoo/addons2
 echo "Copiando archivos filestore..."
-docker exec odoo17 cp -rf /var/lib/odoo2/. /var/lib/odoo
-docker exec odoo17 rm -rf /var/lib/odoo2
+docker exec odoo17ce2025 cp -rf /var/lib/odoo2/. /var/lib/odoo
+docker exec odoo17ce2025 rm -rf /var/lib/odoo2
 
 # Agrega configuracion de Odoo
 sudo cp -f odoo.conf odoo-etc/
@@ -41,18 +41,18 @@ docker compose down
 docker compose up -d
 
 # Verificar si Caddy está instalado
-if ! command -v caddy &> /dev/null
-then
-    echo "Instalando Caddy..."
-    sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
-    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-    sudo apt update
-    sudo apt install caddy
-    sudo cp -f Caddyfile /etc/caddy/
-    sudo systemctl restart caddy
-    echo "Caddy ya está instalado."
-else
-    echo "Caddy ya está instalado."
-fi
+# if ! command -v caddy &> /dev/null
+# then
+#     echo "Instalando Caddy..."
+#     sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+#     curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+#     curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+#     sudo apt update
+#     sudo apt install caddy
+#     sudo cp -f Caddyfile /etc/caddy/
+#     sudo systemctl restart caddy
+#     echo "Caddy ya está instalado."
+# else
+#     echo "Caddy ya está instalado."
+# fi
 
